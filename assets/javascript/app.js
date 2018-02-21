59eb9062c6e83d6da5803f128dc6af184e4cc776
Rifa Jafri <rifajafri@Rifas-MacBook-Pro.local>
2018-02-20 21:25:52 -0800

    added js

---
+++ assets/javascript/app.js
@@ -0,0 +1,311 @@
+
+What team of teenage superheroes sparked the first Civil War?
+Young AvengersPower PackChampions NEW WARRIORS
+The Venom Symbiote is a member of an alien race called the...
+Brood KLYNTAR Badoon Shi'ar
+Peter Quill once had a cybernetic...
+leg EYE brain hand
+The first superhero created in the Marvel universe was...
+The Human TorchCaptain AmericaCaptain Marvel NAMOR THE SUB-MARINER 
+Peter Parker's parents were named...
+Matthew and Linda Parker RICHARD AND MARY PARKER Ben and May ParkerThomas and Martha Parker
+Before becoming a herald of Galactus, the Silver Surfer was known as...
+En Sabah NurTaaNova Prime NORRIN RADD 
+Who came up with the team name 'Avengers'?
+Iron Man WASP Captain America Ant-Man
+Who did Bruce Banner push out of the way when he was hit by the gamma radiation that turned him into the Hulk?
+Amadeus ChoBetty RossRichard Parker RICK JONES
+Who was an original member of the Guardians of the Galaxy?
+DarkhawkMoondragon MAJOR VICTORY Groot
+Who was the first Iron Patriot?
+James RhodesSimon WilliamsTony Stark NORMAN OSBORN
+Sam Wilson's pet falcon is named...
+WashingtonBlackwing REDWING America
+Scarlet Witch and Quicksilver are NOT the children of Magneto, but actually...
+Children of Professor Xavier Experiments by Hydra CREATIONS OF THE HIGH EVOLUTIONARY Inhumans
+Who is the brother of Thanos?
+STARFOX ThaneAnnihilus Titan
+What couple was married in the comics by Stan Lee?
+LUKE CAGE AND JESSICA JONES Peter Parker and Mary Jane WatsonReed Richards and Sue StormDeadpool and Shiklah
+Where did Black Widow receive her training?
+The Russian Room THE RED ROOM HydraS.H.I.E.L.D.
+When S.H.I.E.L.D. was turned into H.A.M.M.E.R., what did the acronym change to?
+Homeland Association for Military Might Enforcement Reserves 
+THEY NEVER CAME UP WITH A NEW ACRONYM Homeland Attack Management for Militant Enemies RegimentHomeland Assembly of Military Maneuvering Engaging Rebels
+After the death of Charles Xavier, the Xavier School is renamed to what?
+The Cyclops SchoolThe X-InstituteLogan's School THE JEAN GREY SCHOOL
+What destroyed the Nova Corps?
+The Kree EmpireThanosGalactus THE ANNIHILATION WAVE 
+What alien race attempted to take over the earth in Secret Invasion?
+THE SKRULLS  The BadoonThe BroodThe Kree
+Who was the first hero killed in Civil War after the passing of the Superhuman Registration Act?
+SpeedballCaptain America GOLIATH Night Thrasher
+Who fought crime as Daredevil While Matt Murdoch was trying to prove that he wasn't the costumed vigilante?
+Spider-ManWinter SoldierMoon Knight IRON FIST 
+Who specializes in Chaos Magic?
+SCARLET WITCH  Doctor DoomDoctor StrangeWiccan
+Who's Death was the catalyst for Civil War II?
+Bruce BannerWasp WAR MACHINE Tony Stark
+The Thunderbolts were originally a team of superheroes comprised of
+UNDERCOVER VILLIANS Heroes with electricity based powersS.H.I.E.L.D. Operatives Teenagers
+Who has NOT been the director of S.H.I.E.L.D.?
+PHIL COULSON Maria HillSteve RogersDaisy Johnson
+Ben Reilly and Kaine Parker, Clones of Peter Parker, both used what title to fight crime?
+The Spider SCARLET SPIDER Tarantula The Jackal
+Who killed Bruce Banner?
+ThanosUltron HAWKEYE Professor Xavier
+What villain was born from subconscious mental parts of Charles Xavier and Magneto?
+Mr. Sinister ONSLAUGHT  AnnihilusApocalypse 
+Deadpool's friend Bob is a member of what evil organization
+A.I.M.The Hand HYDRA Secret Empire
+Where did Peter Parker attend College?
+New York University EMPIRE STATE UNIVERSITY He didn't go to collegeMidtown Prep
+Who Killed Gwen Stacy?
+VenomThe JackalSpider-man GREEN GOBLIN
+Galactus was born on what planet?
+TAA It is unknown where Galactus was bornSpartaxCounter Earth
+Which of these is true about Groot?
+He has a wife He is a mutated earth treeTelepaths cannot read his mind HE COULD ORIGINALLY SPEAK LIKE A NORMAL HUMAN
+Vision was created by
+He built himselfHank Pym ULTRON Tony Stark
+Which mutant was a member of the very first team of X-Men?
+Wolverine ANGEL StormColossus
+How is She-Hulk related to Bruce Banner?
+THEY ARE COUSINS They are siblingsShe is his daughterThey are step-siblings
+Who's mutant ability makes everyone not looking at them forget about their existence?
+ForgetAgain FORGET ME NOT MemoryCortex
+Laura Kinney is a clone of which superhero?
+Spider-Man WOLVERINE Emma FrostCarol Danvers
+What animals pull Thor's chariot?
+Six legged horsesOxenEagles GOATS
+Shuma Gorath is a monster known for fighting what superhero?
+Iron Man DOCTOR STRANGE HulkThor
+Who turned Bucky into the Winter Soldier?
+HydraNick FuryThe Secret Empire THE SOVIETS
+When it was first introduced in the comics, what did S.H.I.E.L.D. stand for?
+Strategic Homeland Intervention, Enforcement and Logistics DivisionIt didn't stand for anythingStrategic Hazard Intervention Espionage Logistics Directorate "Supreme Headquarters, International Espionage, Law-Enforcement Division"
+What event saw the death of Charles Xavier?
+AVENGERS VS XMEN Inhumans Vs. X-MenAxisHouse of M
+Who killed Steve Rogers after the first Civil War?
+Winter SoldierCrossbones SHARON CARTER Iron Man
+Thor's Hammer Mjolnir is made out of what material?
+Nth MetalIt is unknown what it is made of URU damantium
+What two alien races waged war over the planet earth
+The Skrulls and the Annihilation WaveThe Chitauri and the Kree THE KREE AND THE SKRULLS The Shi'ar and the Chitauri
+The Inhumans get their powers from...
+radiationthe X-gene TERRIGEN the metagene
+
+
+var quizContainer;
+var resultsContainer;
+var submitButton;
+
+$("#quiz").append(quizContainer)
+$("#results").append(resultsContainer)
+$("#submit").append(submitButton)
+
+var marvelTrivia = [
+
+    {
+        question: "Which of these is NOT an alias used by Clint Barton?",
+        answers: ["Trickshot", "Goliath", "Hawkeye", "Ronin"],
+        // image:
+        correct: 0
+    },
+    {
+        question: "Who is the richest character in the Marvel Universe?",
+        answers: ["Tony Stark", "King T'Challa", "Professor X", "Reed Richards"],
+        // image:
+        correct: 1
+    },
+    {
+        question: "The Inhumans got their powers from...?",
+        answers: ["Radiation", "The X-Gene", "The Metagene", "Terrigen"],
+        // image:
+        correct: 3
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    },
+    {
+        question: 
+        answers: 
+        image:
+        correct:
+    }
+
+
+
+    
+
+
+
+
+]
+
+
+$(document).on("click", function() {
+    var timeleft = 30;
+        var downloadTimer = setInterval(function(){
+        timeleft--;
+        $("#countdowntimer").html(timeleft);
+        if(timeleft <= 0)
+            // clearInterval(downloadTimer);
+            alert("Time's Up")
+        },1000);
+
+
+})
